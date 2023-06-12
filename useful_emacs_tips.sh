9a2e8b75bb8bbe207d638833e2a34d765090b1ea
Yoav Avitzour <yavitzour@gmail.com>
2023-06-12 12:32:46 +0300

    added font installation instructions for emacs

--- useful_emacs_tips.sh
+++ useful_emacs_tips.sh
@@ -2,6 +2,17 @@
 
 # Install doom emacs in a special folder
 
-name=default
+name=full
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d.doom-${name}
-DOOMDIR=~/.doom.d.${name} ~/.emacs.d.doom-${name}/bin/doom install
+DOOMDIR=~/doom_config/doom.d.${name} ~/.emacs.d.doom-${name}/bin/doom install
+
+name=thin
+git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d.doom-${name}
+DOOMDIR=~/doom_config/doom.d.${name} ~/.emacs.d.doom-${name}/bin/doom install
+
+# REMEMBER TO INSTALL FONTS!!! (SourceCodePro)
+mkdir -p ~/INSTALL/Fonts
+cd ~/INSTALL/Fonts
+git clone https://github.com/adobe-fonts/source-code-pro.git
+cp source-code-pro/OTF/* ~/.local/share/fonts/
+fc-cache -f -v
